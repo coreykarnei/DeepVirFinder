176dbbe66d09e02a7f5c894d7f8ca310ee600fe0
Corey K <coreykarnei@utexas.edu>
2022-04-28 16:25:57 -0500

    human and covid results

--- run_dvf_over_folder.sh
+++ run_dvf_over_folder.sh
@@ -1,1 +1,11 @@
-echo $1
+src_dir=$1
+
+for fname in "$src_dir"/*
+do
+    # echo $fname
+    arrIN=(${fname//// })
+    # # echo ${arrIN[0]}    
+    out="./results/${arrIN[2]}"
+    # echo $out  
+    python dvf.py -i $fname -o $out
+done
