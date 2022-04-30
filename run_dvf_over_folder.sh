6fdee1d1e5b3301756bfcb8c7361fdb37a06d041
Corey K <coreykarnei@utexas.edu>
2022-04-30 02:58:57 -0500

    DeepVirFinder results over large dataset

--- run_dvf_over_folder.sh
+++ run_dvf_over_folder.sh
@@ -1,11 +1,22 @@
 src_dir=$1
 
-for fname in "$src_dir"/*
+# for nested folders
+for fname in "$src_dir"/*/*
 do
-    # echo $fname
     arrIN=(${fname//// })
-    # # echo ${arrIN[0]}    
-    out="./results/${arrIN[2]}"
-    # echo $out  
-    python dvf.py -i $fname -o $out
+    echo "In: $fname"
+    echo "Out: ./results/${arrIN[2]}/${arrIN[3]}"   
+    python dvf.py -i $fname -o "./results/${arrIN[2]}"
 done
+
+
+# for one folder
+# for fname in "$src_dir"/*
+# do
+#     # echo $fname
+#     arrIN=(${fname//// })
+#     # # echo ${arrIN[0]}    
+#     out="./results/${arrIN[2]}"
+#     # echo $out  
+#     python dvf.py -i $fname -o $out
+# done
